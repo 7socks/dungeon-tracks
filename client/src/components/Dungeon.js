ff59bd4fb684968304b088ebdb6010dce08a1d22
Elfric <elf72742@gmail.com>
2022-08-10 13:23:41 -0400

    dungeon title editing focus/blur behavior

--- client/src/components/Dungeon.js
+++ client/src/components/Dungeon.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { HiPencil } from 'react-icons/hi';
+import { ImCheckmark } from 'react-icons/im';
 
 import { PlaylistControls } from './AudioControls';
 import Playlist from './Playlist';
@@ -11,32 +13,64 @@ const DungeonContainer = styled.div`
   justify-content: space-around;
   width: 100%;
 
+  button:not(.muffle-btn) {
+    border: none;
+    background: none;
+    color: var(--theme-btn-text-dim);
+
+    :hover {
+      color: var(--theme-btn-text-undim);
+    }
+  }
+`;
+
+const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 1.2em 0 1.2em 1em;
+
   h1 {
-    width: 100%;
+    margin: 0;
+  }
+
+  button {
+    font-size: 16px;
   }
 `;
 
 const DungeonTitle = ({title, update}) => {
+  const [focused, setFocused] = useState(false);
   const [editing, setEditing] = useState(false);
   const [editTitle, setEditTitle] = useState(title);
+
+  useEffect(() => {
+    setFocused(editing);
+  }, [editing]);
 
   const edit = (e) => {
     setEditTitle(e.target.value);
   };
 
+  const handleBlur = () => {
+    setEditing(false);
+  };
+
   if (!editing) {
-    return <h1>{title}</h1>
+    return <TitleContainer>
+      <h1>{title}</h1>
+      <button onClick={() => setEditing(true)}>
+        <HiPencil/>
+      </button>
+    </TitleContainer>;
   } else {
-    return (<div>
-      <input type="text" value={editTitle} onChange={edit}/>
+    return (<TitleContainer onBlur={focused ? handleBlur : null}>
+      <input type="text" value={editTitle} onChange={edit} autoFocus/>
       <button onClick={() => {
         setEditing(false);
         update(editTitle);
-      }}>V</button>
-      <button onClick={() => {
-        setEditing(false);
-      }}>X</button>
-    </div>);
+      }}><ImCheckmark/></button>
+    </TitleContainer>);
   }
 };
 
